9eb6ec4382244154809d920eec4d0c46f9475d20
dcarav77 <dcarav77@gmail.com>
2026-05-20 15:11:50 +0800

    comment changes

--- rtl/learning/top_module.sv
+++ rtl/learning/top_module.sv
@@ -9,12 +9,10 @@ module top_module (
 
     blinky dut1 (
         .clk(clk),
-        .rst(rst),
-        
-        //.enable(enable), Use this if you want a phyical switch/button to control enable.       
-        .enable(1'b1), 
-        .JA1(),                       //not connected
-        .pulse(pulse_from_blinky)    //output goes to internal
+        .rst(rst),                 
+        .enable(1'b1),              //.enable(enable) If you wanted a phyical switch/button to control enable. 
+        //.JA1(),                   //not connected
+        .pulse(pulse_from_blinky)   //output goes to internal
     );
 
 
